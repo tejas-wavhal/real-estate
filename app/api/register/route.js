bfe65af15365aa39a03c6cd6dffde82fc0ed66fa
Tejas <twavhal7514@gmail.com>
2024-01-21 14:27:12 +0530

    implemented next auth

---
+++ app/api/register/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import prisma from "@/utils/prismaDB";
+
+export async function POST(request) {
+  const body = await request.json();
+  const { email, name, password } = body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const user = await prisma.user.create({
+    data: { email, name, hashedPassword }
+  });
+
+  return NextResponse.json(user);
+}
